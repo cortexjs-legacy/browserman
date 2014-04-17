53dbb711d797470de3e20a82b10a4855299c10d4
ltebean <yucong1118@gmail.com>
2014-04-17 13:36:54 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var stylus = require('gulp-stylus');
+var jade = require('gulp-jade');
+
+process.on("uncaughtException",function(err){
+    console.log(err);
+});
+
+gulp.task('stylus', function(){
+    gulp.src(["./public/css/**/*.styl"])
+        .pipe(stylus())
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('jade', function(){
+    gulp.src(["./public/views/*.jade"])
+        .pipe(jade())
+        .pipe(gulp.dest("./public/html/"));
+
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch(["./public/views/**/*.jade"], ['jade']);
+  gulp.watch(["./public/css/**/*.styl"], ['stylus']);
+});
+
+gulp.task('default', ['stylus','jade','watch']);
