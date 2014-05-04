e2e14b0e46e400ed62c414abd778f96b3aa0bace
ltebean <yucong1118@gmail.com>
2014-05-04 10:17:57 +0800

    fix reconnection

--- public/js/dev/browser.js
+++ public/js/dev/browser.js
@@ -1,24 +1,21 @@
 var io = require('./lib/socket.io');
-var browser=require('bowser').browser;
+var browser = require('bowser').browser;
 
 var socket = io.connect('/worker', {});
 
 socket.on('connect', function() {
 	console.log('connected');
-
 	socket.emit('register', {
 		name: browser.name,
 		version: browser.version
 	});
+});
 
-	socket.on('job', function(job) {
-		console.log('job arrive')
-		window.open(job.url);
-	});
+socket.on('job', function(job) {
+	console.log('job arrive')
+	window.open(job.url);
+});
 
-	socket.on('disconnect', function() {
+socket.on('disconnect', function() {
 
-	});
-
-
-})
+});
