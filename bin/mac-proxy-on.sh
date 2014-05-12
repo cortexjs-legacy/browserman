db233127e6f1185ff010ff4048ed8f01f28710d7
ltebean <yucong1118@gmail.com>
2014-05-12 15:02:07 +0800

    improve browser options fixed #2

--- bin/mac-proxy-on.sh
+++ bin/mac-proxy-on.sh
@@ -1,5 +1,5 @@
-echo "set wifi proxy to browserman.dp:9001"
-networksetup -setwebproxy wi-fi browserman.dp 9001
+echo "set wifi proxy to localhost:9001"
+networksetup -setwebproxy wi-fi localhost 9001
 echo "turning on proxy..."
 networksetup -setwebproxystate wi-fi on
 echo "done"
